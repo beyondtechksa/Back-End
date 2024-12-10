e9afa5eebe08d8bace7873360bd2aa4c63219f94
maged <maged.hamed@orgate.io>
2024-12-10 18:03:50 +0200

    Verify mobile app account using code

---
+++ app/Notifications/CustomEmailVerificationMobile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CustomEmailVerificationMobile extends VerifyEmail
+{
+    public $verificationCode;
+
+    public function __construct($verificationCode)
+    {
+        $this->verificationCode = $verificationCode;
+    }
+
+    /**
+     * Get the notification's mail representation.
+     *
+     * @param mixed $notifiable
+     */
+
+    public function toMail($notifiable): MailMessage
+    {
+
+        return (new MailMessage)
+            ->subject('Hello!')
+            ->line('Please use the following code to verify your email address:')
+            ->line('Verification Code: ' . $this->verificationCode)
+            ->line('If you did not create an account, no further action is required.')
+            ->salutation('Regards, Beyond Store');
+    }
+}
